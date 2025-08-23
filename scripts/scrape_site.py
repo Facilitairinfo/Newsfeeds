0fc42ff7237050ac53a4d2523a40cc9405d3428f
Facilitairinfo <info@facilitairnetwerk.com>
2025-08-23 12:46:45 +0200

    Update and rename scrape_site.py to scraper.py

--- scripts/scrape_site.py
+++
@@ -1,8 +0,0 @@
-published = parse_event_date(
-    block,
-    date_selector=site.get("date_selector"),
-    date_attr=site.get("date_attr"),
-    fallback_day_selector=site.get("fallback_day_selector"),
-    fallback_month_selector=site.get("fallback_month_selector"),
-    fallback_year_selector=site.get("fallback_year_selector")
-)
